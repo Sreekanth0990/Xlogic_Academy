96eb961bcd23b011b5cb8fc1d783449d6e9cf40a
Sreekanth0990 <88582793+Sreekanth0990@users.noreply.github.com>
2024-07-02 08:59:55 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -104,6 +104,105 @@
             }
         }
     });
+
+    $(document).ready(function(){
+        // Add minus icon for collapse element which is open by default
+        $(".collapse.show").each(function(){
+            $(this).siblings(".card-header").find(".btn i").addClass("fa-minus-circle").removeClass("fa-plus-circle");
+        });
+        
+        // Toggle plus minus icon on show hide of collapse element
+        $(".collapse").on('show.bs.collapse', function(){
+            $(this).parent().find(".card-header .btn i").removeClass("fa-plus-circle").addClass("fa-minus-circle");
+        }).on('hide.bs.collapse', function(){
+            $(this).parent().find(".card-header .btn i").removeClass("fa-minus-circle").addClass("fa-plus-circle");
+        });
+
+
+        
+        $('#enrollmentForm').submit(function(event) {
+            
+            event.preventDefault(); // Prevent the form from submitting
+
+            // Get form values
+            var fullName = $('#fullName').val();
+            var email = $('#email').val();
+            var mobNumber = $('#mobNumber').val();
+            var course = $('#course').val();
+
+            // Store data in sessionStorage (or you can use localStorage if you want data to persist)
+            sessionStorage.setItem('fullName', fullName);
+            sessionStorage.setItem('email', email);
+            sessionStorage.setItem('mobNumber', mobNumber);
+            sessionStorage.setItem('course', course);
+
+            console.log("Enrollment Data:", sessionStorage.getItem('fullName'));
+
+
+            $('#fullname').val('');
+            $('#mobnumber').val('');
+            $('#email').val('');
+
+            // Redirect to confirmation page
+            window.location.href = 'payment.html';
+        });
+
+
+        // Add custom method to validate letters and spaces only
+    jQuery.validator.addMethod("lettersonly", function(value, element) {
+        return this.optional(element) || /^[a-zA-Z\s]*$/.test(value);
+    }, "Letters and spaces only please");
+
+    // Validate the form on submit
+    $('#enrollmentForm').validate({
+        rules: {
+            fullName: {
+                required: true,
+                lettersonly: true
+            },
+            email: {
+                required: true,
+                email: true
+            },
+            mobNumber: {
+                required: true,
+                digits: true,
+                minlength: 10,
+                maxlength: 10
+            },
+            course: {
+                required: true
+            }
+        },
+        messages: {
+            fullName: {
+                required: "Please enter your full name"
+            },
+            email: {
+                required: "Please enter your email address",
+                email: "Please enter a valid email address"
+            },
+            mobileNumber: {
+                required: "Please enter your mobile number",
+                digits: "Please enter only digits",
+                minlength: "Please enter at least 10 digits",
+                maxlength: "Please enter no more than 10 digits"
+            },
+            courseName: {
+                required: "Please enter the course name"
+            }
+        },
+        submitHandler: function(form) {
+            // Form is valid, submit it
+            form.submit();
+        }
+    });
+
+
+
+    });
+
+    
     
 })(jQuery);
 
